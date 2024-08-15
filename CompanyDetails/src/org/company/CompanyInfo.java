e46853459ebd459d67aeee1ed17e88761f5ff41a
dell <dell@DESKTOP-GJ0BOG6>
2024-08-15 21:10:27 +0530

    First

---
+++ CompanyDetails/src/org/company/CompanyInfo.java
@@ -0,0 +1,25 @@
+package org.company;
+
+public class CompanyInfo {
+
+	private void companyName() {
+		System.out.println("greens");
+
+	}
+	private void companyId() {
+		System.out.println("CHR5643");
+
+	}
+	private void companyAddress() {
+		System.out.println("2/345 north street,chennai");
+
+	}
+	public static void main(String[] args) {
+		CompanyInfo a = new CompanyInfo();
+		a.companyName();
+		a.companyId();
+		a.companyAddress();
+		
+	}
+}	
+
